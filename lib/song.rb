e693cd30c80ea3956037ff8369d1a71a96cfe901
Daniel Lee <leedc@me.com>
2020-03-23 00:32:01 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,6 @@
+# require 'pry'
+# binding.pry
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -8,6 +11,40 @@ class Song
 
   def save
     self.class.all << self
+  end
+
+  def self.create
+    song = Song.new
+    song.save
+    song
+  end
+
+  def Song.new_by_name(song_name)
+    song = self.new
+    song.name = song_name
+    song
+  end
+
+  def Song.create_by_name(song_name)
+    song = self.create
+    song.name = song_name
+    song
+  end
+
+  def Song.find_by_name(song_name)
+    self.all.find{ |song| song.name == song_name }
+  end
+
+  def Song.find_or_create_by_name(song_name)
+    self.find_by_name(song_name) || self.create_by_name(song_name)
+  end
+
+  def Song.alphabetical
+    self.all.sort_by{ |s| s.name}
+  end
+
+  def Song.new_from_filename(song_name)
+
   end
 
 end
